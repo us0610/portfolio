ac1a4b619a9e84328a0c5e13c9e77609a24efbf2
us0160 <ujjwal158b@gmail.com>
2022-10-29 11:04:39 +0530

    added projects

--- src/sections/projects/ProjectsList.js
+++ src/sections/projects/ProjectsList.js
@@ -1,4 +1,14 @@
 import honeyImg from "../../assets/honeybeeyourself.gif";
+import webNovel from "../../assets/webnovel.gif";
+import qualia from "../../assets/qualia.gif";
+import astq from "../../assets/astq.gif";
+import reactMovies from "../../assets/react-movies.gif";
+import freewillie from "../../assets/free-wilie.gif";
+import realtor from "../../assets/realtor.gif";
+import metabeats from "../../assets/metabeats.gif";
+import tumtumImg from "../../assets/tumtum.gif";
+import metamonstersImg from "../../assets/metamonster.gif";
+import influencerImg from "../../assets/influencer.gif";
 
 const PROJECTS = [
   {
@@ -7,6 +17,76 @@ const PROJECTS = [
     desc: "Honey bee yourself is an e-commerce store build in webflow using webflow’s CMS and custom javascript. It has custom menu and decent animations.",
     link: "https://www.honeybeeyourself.com/",
     img: honeyImg,
+  },
+  {
+    id: "webnovel21",
+    name: "Web Novel",
+    desc: "A template for authors to upload their novels/comics. This project is made in webflow and is heavily custom coded. It includes sorting, dynamic slider and Finsweet library to enhance the CMS of webflow platform.",
+    link: "https://web-novel.webflow.io/",
+    img: webNovel,
+  },
+  {
+    id: "qualia22j",
+    name: "Qualia",
+    desc: "A NFT website made in webflow. It includes custom menu and decent animations.",
+    link: "https://www.qualianft.xyz/",
+    img: qualia,
+  },
+  {
+    id: "metabeats22",
+    name: "Metabeats",
+    desc: "A NFT website built in webflow. This project includes custom menu and webflow's CMS functionality.",
+    link: "https://www.metabeats.studio/",
+    img: metabeats,
+  },
+  {
+    id: "tumtum22",
+    name: "Tum Tum NFT",
+    desc: "A NFT website built in webflow. It's heavily custom coded and also has minting functionality, you can connect your wallet and mint the NFT directly from the website.",
+    link: "https://www.tumtumnft.com/",
+    img: tumtumImg,
+  },
+  {
+    id: "artsavethequeen22",
+    name: "Art Save The Queen",
+    desc: "A complex layout NFT website build in webflow, with a bit of custom javascript.",
+    link: "https://www.artsavethequeen.com/",
+    img: astq,
+  },
+  {
+    id: "meatmonster21",
+    name: "Meta Monsters",
+    desc: "A NFT website built in webflow. It has custom coded navbar for which I used GSAP library.",
+    link: "https://www.metamonsters.party/",
+    img: metamonstersImg,
+  },
+  {
+    id: "reactmovies21",
+    name: "React Movies",
+    desc: "A movies app built with ReactJs. You can search, review and see popular movies with this app. User functionality is still in development but will be added soon.",
+    link: "https://react-moviesus.netlify.app/",
+    img: reactMovies,
+  },
+  {
+    id: "influencerlist21",
+    name: "Influencer Template",
+    desc: "A template made in webflow. This template can be customised into 3 different types. It also has dark theme and custom hover animations.",
+    link: "https://influencerlist-6e8e25a3fa744a5c9155da42.webflow.io/",
+    img: influencerImg,
+  },
+  {
+    id: "freewillie21",
+    name: "Free Willie",
+    desc: "A NFT website made in webflow with custom code, smooth animations.",
+    link: "https://free-willie.netlify.app/",
+    img: freewillie,
+  },
+  {
+    id: "realtor22",
+    name: "Realtor",
+    desc: "A RealEstate app built with nextjs. You can search for properties, sort them based on price, room number, space etc. It's has good SEO support as it's build with nextjs.",
+    link: "https://realtor-app-beta.vercel.app/",
+    img: realtor,
   },
 ];
 
